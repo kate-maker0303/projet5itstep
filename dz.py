54b10296061c4786020712835047c9d28ec0482a
Home <tatusya0303@gmail.com>
2026-05-02 10:58:59 +0300

    створена робота

---
+++ dz.py
@@ -0,0 +1,43 @@
+import colorama
+from colorama import Fore, Back, Style
+
+help(colorama)
+def func():
+    pass
+
+class Human:
+    pass
+
+color = colorama
+f1 = func
+roma = Human
+
+print(colorama.__name__)
+print(color.__name__)
+print(f1.__name__)
+print(func.__name__)
+print(Human.__name__)
+print(roma.__name__)
+romm = Human()
+type(romm)
+print(type(romm))
+
+str1 = "person"
+print(hasattr(str1, "reverse"))
+print(hasattr(str1, "index"))
+print(hasattr(str1, "upper"))
+print(hasattr(str1, "lower"))
+print(hasattr(str1, "title"))
+
+var = colorama.Fore
+var = Fore.GREEN
+print(var, "Hello World")
+varr = colorama.Back
+varr = Back.RED
+print(varr, "Hello World")
+varrr = colorama.Style
+varrr = Style.BRIGHT
+print(varrr, "Hello World")
+
+#Це бібліотекаб яка дозволяє робити кольоровий текст у консолі
+#Особливо корисна для Windows, де стандартні ANSI-коди не працюють
